fc094d8c5ffe56486c50ce25ae5d413bf88781ba
Rushi_Gandhi <rushigandhi1109@gmail.com>
2022-12-11 00:11:30 +0530

    Initial Commit

--- app/src/main/java/com/example/digitalexpensetracker/AddTransactionActivity.java
+++ app/src/main/java/com/example/digitalexpensetracker/AddTransactionActivity.java
@@ -3,12 +3,31 @@ package com.example.digitalexpensetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddTransactionActivity extends AppCompatActivity {
+
+    private Spinner spinnerCategory;
+    private ArrayAdapter<CharSequence> categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
+
+        getSupportActionBar().setTitle("Add Transaction");
+
+        // Get spinner by id
+        spinnerCategory = findViewById(R.id.spinner_category);
+
+        // Populate arrayAdapter using string array
+        categoryAdapter = ArrayAdapter.createFromResource(AddTransactionActivity.this, R.array.array_categories, R.layout.spinner_layout);
+
+        // Specify the layout to use when the list of choice appear
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the adapter to spinner to populate the category spinner
+        spinnerCategory.setAdapter(categoryAdapter);
     }
 }
--- app/src/main/java/com/example/digitalexpensetracker/DashboardActivity.java
+++ app/src/main/java/com/example/digitalexpensetracker/DashboardActivity.java
@@ -2,9 +2,14 @@ package com.example.digitalexpensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private RelativeLayout RLAddTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,14 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         getSupportActionBar().setTitle("Dashboard");
+
+        RLAddTransaction = findViewById(R.id.RL_add_transaction_button);
+
+        RLAddTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, AddTransactionActivity.class));
+            }
+        });
     }
 }
